63ce3371d26505c319435fdfc2d8c7ed92893491
rangamboa <rangamboa@gmail.com>
2021-07-07 13:22:32 -0700

    updating exercise calculation function

--- js/exercise-search.js
+++ js/exercise-search.js
@@ -9,31 +9,32 @@ var numCals = 0;
 
 var form = $('form');
 var input = $('#workoutInput');
-var result = $('.result');
 var workoutDisp = $('#exerciseDisplay');
 var totalDisp = $('#totalDisplay');
 
-
+// Capture workout information from form.
 form.on('submit', function(event) {
 
   event.preventDefault();
 
-  console.log('search function called.');
-
+  // Assign value to queryItem to use in API.
   queryItem = input.val();
 
-  console.log(queryItem);
-
+  // Call function to make API request.
   makeRequest(queryItem);
+
+  // Clear value of input box.
   input.val('');
 
 });
 
 function makeRequest(queryItem) {
 
+  // This needs to be further modified with user information (height, weight, age)
   requestUrl = url + '&gender=female&age=35';
-  console.log(requestUrl);
-  console.log(queryItem);
+  
+  // console.log(requestUrl);
+  // console.log(queryItem);
 
   fetch(requestUrl, {
     method: 'POST',
@@ -49,34 +50,44 @@ function makeRequest(queryItem) {
   .then(response => response.json())
   .then(data => { 
 
+    // Assigns pulled data to "workouts" to be parsed and used.
     workouts = data;
-    console.log(workouts);
 
-    console.log('types of workouts: '+ workouts.exercises.length);
+    // Console logging for tracking and checking variables.
 
+    // console.log(workouts);
+    // console.log('types of workouts: '+ workouts.exercises.length);
+
+    // for (i=0; i< workouts.exercises.length; i++) {
+    //   console.log('----------');
+    //   console.log((i+1) + ') ' + workouts.exercises[i].name);
+    //   console.log('length of workout (min): ' + workouts.exercises[i].duration_min);
+    //   console.log('calories burned (cal): ' + workouts.exercises[i].nf_calories);
+    // }
+
+    // Loop through number of workouts.
     for (i=0; i< workouts.exercises.length; i++) {
-      console.log('----------');
-      console.log((i+1) + ') ' + workouts.exercises[i].name);
-      console.log('length of workout (min): ' + workouts.exercises[i].duration_min);
-      console.log('calories burned (cal): ' + workouts.exercises[i].nf_calories);
-    }
 
- 
-  for (i=0; i< workouts.exercises.length; i++) {
+      // Increase workout total for (display purposes only).
+      numWorkouts++;
 
-    numWorkouts++;
+      // Update workout list display.
+      workoutDisp.append('✅ Workout #' + numWorkouts + ', ' + workouts.exercises[i].name + ' for ' + + workouts.exercises[i].duration_min + ' minutes, burns ' + Math.floor(workouts.exercises[i].nf_calories) +' calories.<br />');
 
-    workoutDisp.append('✅ Workout #' + numWorkouts + ', ' + workouts.exercises[i].name + ' for ' + + workouts.exercises[i].duration_min + ' minutes, burns ' + Math.floor(workouts.exercises[i].nf_calories) +' calories.<br />');
+      // Round calories down for readability.
+      numCals+=Math.floor(workouts.exercises[i].nf_calories);
 
-    numCals+=Math.floor(workouts.exercises[i].nf_calories);
-
-    totalDisp.empty();
-    totalDisp.append('💪 Total Workouts: '+ numWorkouts);
-    totalDisp.append('<br />');
-    totalDisp.append('🔥 Total Calories Burned: '+ numCals);
-
+      // Update running display of total workouts and total calories burned.
+      totalDisp.empty();
+      totalDisp.append('💪 Total Workouts: '+ numWorkouts);
+      totalDisp.append('<br />');
+      totalDisp.append('🔥 Total Calories Burned: '+ numCals);
   }
 
+  // Keep track of numCals (total calories burned).
+  console.log(numCals);
+
+  // Pass numCals to another function WITHIN this fetch request.
 
   });
 

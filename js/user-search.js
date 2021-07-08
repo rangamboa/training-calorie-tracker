b4af6aeb672e4805a4b834140839ee376e24604d
rangamboa <rangamboa@gmail.com>
2021-07-08 11:42:01 -0700

    integrated all js files into one

--- js/user-search.js
+++ js/user-search.js
@@ -5,7 +5,9 @@ var htInItem = $('.usersHtin');
 var weightItem = $('.usersWeight');
 var saveButton = $('#saveBtn');
 var enterButton = $('#enterBtn');
-var inputEl = $('input');
+var okay = 1;
+var calcHeight;
+var calcWeight;
 
 function createUser() {
     const user = localStorage.setItem(nameItem, ageItem, heightItem, weightItem);
@@ -33,34 +35,46 @@ enterButton.on('click', function(event) {
     event.preventDefault();
     console.log('User Info should have been entered at this point.\nNeed to check for validity.');
 
-    // Convert height in feet/inches to centimeters to satisfy API query parameter requirement.
+    // Check for valid input values.
 
-    console.log(inputEl.length);
-
-    console.log(ageItem[0].value);
-
-    if (ageItem[0].value) {
+    if (ageItem[0].value <= 0) {
         alert('Please enter a valid number for age.');
         ageItem[0].value = '';
         ageItem[0].style = 'background-color: pink;';
+        okay = 0;
     }
     if (htFtItem[0].value <= 0) {
         alert('Please enter a valid height in feet.');
-        ageItem[0].value = '';
-        ageItem[0].style = 'background-color: pink;';
+        htFtItem[0].value = '';
+        htFtItem[0].style = 'background-color: pink;';
     }
-    if (htInItem[0].value <= 0) {
-        alert('Please enter a valid number.');
-        ageItem[0].value = '';
-        ageItem[0].style = 'background-color: pink;';
+    if (htInItem[0].value < 0 || htInItem[0].value > 12) {
+        alert('Please enter a valid height in inches.');
+        htInItem[0].value = '';
+        htInItem[0].style = 'background-color: pink;';
     }
     if (weightItem[0].value <= 0) {
-        alert('Please enter a valid number.');
-        ageItem[0].value = '';
-        ageItem[0].style = 'background-color: pink;';
+        alert('Please enter a valid weight in pounds.');
+        weightItem[0].value = '';
+        weightItem[0].style = 'background-color: pink;';
     }
 
-    // Convert weight in pounds to kilograms to satisfy API query parameter requirement.
+    // If all inputs are valid, proceed.
+    if (okay == 1) {
 
+        // Convert height in feet/inches to centimeters to satisfy API query parameter requirement.
+
+        // First, convert to height in inches.
+        calcHeight = (parseInt(htFtItem[0].value)*12)+parseInt(htInItem[0].value);
+        console.log(calcHeight);
+
+        // Then, convert to centimeters.
+        calcHeight = Math.floor(calcHeight*2.54);
+        console.log(calcHeight);
+
+        // Convert weight in pounds to kilograms to satisfy API query parameter requirement.
+        calcWeight = Math.floor(parseInt(weightItem[0].value)/2.205);
+        console.log(calcWeight);
+    }
 });
 
